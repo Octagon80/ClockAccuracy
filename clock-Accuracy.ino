21fac5ef6411b0430098faf23d13b96623f139c0
Octagon80 <octagon80@mail.ru>
2021-10-28 15:05:39 +0500

    Тестовая версия

--- clock-Accuracy.ino
+++ clock-Accuracy.ino
@@ -22,6 +22,8 @@
 #include "Adafruit_GFX.h"
 #include "Adafruit_SSD1306.h"
 
+
+#define DISPLAY_TIMEOUT 1000000
 #define led_out 13
 #define sensor_in 2
 
@@ -34,28 +36,61 @@ uint32_t mytime = 0 ;
 uint32_t oldtime = 0;
 uint32_t diftime = 0;
 
-uint32_t tmr, tmrold = micros();
-uint32_t dispSek, dispmSek;
-float HZ;
+uint32_t displayTimer, displayTimerOld = micros();
 
 
-void setup() {
+
+void displayInit(){
    display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // указываем адрес устройства на шине
-display.clearDisplay();
+   display.clearDisplay();
    display.setTextSize(1, 2); // указываем размер шрифта
    display.setTextColor(SSD1306_WHITE); // указываем цвет надписи
 
    display.setCursor(30, 10);
    display.println("Clock accuracy");
    display.display();
-    delay(3000);
-     display.clearDisplay(); // очищаем экран
+   delay(3000);
+   display.clearDisplay(); // очищаем экран
+}
 
+
+
+/**
+ * Отобразить на экране параметры 
+ * Обновления экрана не чаще DISPLAY_TIMEOUT-uСек
+ * @param period_us  uint32_t период колебания маятника, микросекунды
+ */
+void displayParam(uint32_t period_us){
+  uint32_t dispSek, dispmSek;
+  float f;
+  //char str[40];
   
+  
+    displayTimer = micros();
+    if( displayTimerOld - displayTimer > DISPLAY_TIMEOUT ){
+     display.clearDisplay();
+     f = period_us; f = (float) (f / 1000000 );
+     display.setCursor(0, 0);  
+     display.print(f);display.print(" cek"); 
+     //dtostrf(f, 4, 2, str); display.print( str );display.print(" cek");
+     f = diftime; f = (float) (1000000 / f );
+     display.setCursor(0, 17); display.print(f); display.print(" Hz");
+     display.display();
+     displayTimerOld = displayTimer;
+    }
+}
+
+
+
+
+void setup() {
+   displayInit();
   // put your setup code here, to run once:
   pinMode (led_out, OUTPUT);
   pinMode (sensor_in, INPUT);
 }
+
+
 
 void loop() {
 
@@ -67,26 +102,10 @@ void loop() {
   }
   else if ((val == LOW) && (oldval == HIGH))  {
     digitalWrite (led_out, LOW);
-    
-      mytime = micros();
-      diftime = mytime - oldtime;      
-      
-    tmr = micros();
-    if( tmrold - tmr > 1000000 ){
-      dispSek = (uint32_t)diftime / 1000000;
-      dispmSek = (uint32_t) (diftime - (dispSek * 1000000));
+    mytime = micros();
+    diftime = mytime - oldtime;      
 
-       HZ = diftime; HZ = (float) (1000000 / HZ );
-       
-     display.clearDisplay();
-     display.setCursor(0, 0);  display.print(dispSek); display.print(".");  display.print(dispmSek); display.print(" cek"); 
-     display.setCursor(0, 17); display.print(HZ); display.print(" Hz");
-     display.display();
-     tmrold = tmr;
-    }
-
-
-   
     oldtime = mytime;
+    displayParam( diftime );
   }
 }
